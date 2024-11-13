c5b476b5758f4073f935888b5020e0d456011134
Alex H <galactionov94@gmail.com>
2024-11-13 10:22:45 -0500

    Task 4. Cloud Testing

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver import FirefoxProfile
+# from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,7 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.options import Options
 
 from app.application import Application
 
@@ -45,26 +45,27 @@ def browser_init(context, scenario_name):
     #     service=service
     # )
     ## HEADLESS MODE FOR FIREFOX####
-    options = Options()
-    options.add_argument('--headless')
-    service = Service(GeckoDriverManager().install())
-    context.driver = webdriver.Firefox(service=service, options=options)
+    # options = Options()
+    # options.add_argument('--headless')
+    # service = Service(GeckoDriverManager().install())
+    # context.driver = webdriver.Firefox(service=service, options=options)
 
     ### BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    # bs_user = 'oleksiihalaktion_xqs9Ku'
-    # bs_key = 'rqsmdpydZ9zzXe9JvfKQ'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    #
-    # options = Options()
-    # bstack_options = {
-    #     "os" : "Windows",
-    #     "osVersion" : "11",
-    #     'browserName': 'edge',
-    #     'sessionName': scenario_name,
-    # }
-    # options.set_capability('bstack:options', bstack_options)
-    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    bs_user = 'oleksiihalaktion_xqs9Ku'
+    bs_key = 'rqsmdpydZ9zzXe9JvfKQ'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os" : "OS X",  # Windows / OS X /
+        "osVersion" : "Big Sur",    # 11 / 10 - For Windows. Sequoia / Ventura ... / - for OS X(Mac)
+        'browserName': 'Firefox',
+        'sessionName': scenario_name,
+    }
+
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(5)
